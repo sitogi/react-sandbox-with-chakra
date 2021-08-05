d660006078eac783e2fffbb5794e866d5e50b351
sitogi <takano162@gmail.com>
2021-08-05 22:37:47 +0900

    Add ResizableSidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import { globalStyle } from 'style';
 import { Box } from '@chakra-ui/react';
+import { ResizableSidebar } from 'components/ResizableSidebar';
 
 function App() {
   return (
     <>
       <Global styles={globalStyle} />
       <Box display="flex" w="100vw" alignItems="center" flexDirection="column" py="100px">
-        sample
+        <ResizableSidebar />
       </Box>
     </>
   );
---
+++ src/components/ResizableSidebar.tsx
@@ -0,0 +1,59 @@
+import { css } from '@emotion/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
+
+const divider = css`
+  margin-left: 10px;
+  margin-right: 10px;
+  width: 3px;
+  height: 100%;
+  border-left-width: 1px;
+  border-left-style: solid;
+  border-left-color: black;
+  border-right-width: 1px;
+  border-right-style: solid;
+  border-right-color: black;
+  cursor: ew-resize;
+  user-select: none;
+`;
+
+export const ResizableSidebar = () => {
+  const isMousePressedRef = useRef(false);
+  const dividerRef = useRef<HTMLDivElement>(null);
+  const [leftW, setLeftW] = useState(200);
+
+  useEffect(() => {
+    document.addEventListener('mouseup', () => {
+      isMousePressedRef.current = false;
+    });
+
+    document.addEventListener('mousemove', (event) => {
+      if (isMousePressedRef.current) {
+        if (dividerRef.current) {
+          if (event.x - 354 < 100) {
+            setLeftW(100);
+          } else if (event.x - 354 > 400) {
+            setLeftW(400);
+          } else {
+            setLeftW(event.x - 354);
+          }
+        }
+      }
+    });
+
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <Flex w="1200px" h="720px" bg="gray.200" p="10px">
+      <Box w={`${leftW}px`}>
+        Aside
+      </Box>
+      <Box ref={dividerRef} css={divider} onMouseDown={() => {
+        isMousePressedRef.current = true;
+      }} />
+      <Box w={`${1200 - leftW}px`}>
+        Main
+      </Box>
+    </Flex>
+  );
+};
