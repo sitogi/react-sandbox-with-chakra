24b34dab9c195b08355802ba2e79818a08a70e2a
sitogi <takano162@gmail.com>
2021-08-05 23:02:55 +0900

    Add VerticalResizable

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import { globalStyle } from 'style';
 import { Box } from '@chakra-ui/react';
-import { ResizableSidebar } from 'components/ResizableSidebar';
+import { VerticalResizable } from 'components/VerticalResizable';
 
 function App() {
   return (
     <>
       <Global styles={globalStyle} />
-      <Box display="flex" w="100vw" alignItems="center" flexDirection="column" py="100px">
-        <ResizableSidebar />
+      <Box display="flex" w="100vw" alignItems="center" flexDirection="column">
+        <VerticalResizable />
       </Box>
     </>
   );
--- src/components/VerticalResizable.tsx
+++ src/components/VerticalResizable.tsx
@@ -3,24 +3,21 @@ import { Box, Flex } from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
 
 const divider = css`
-  margin-left: 10px;
-  margin-right: 10px;
-  width: 3px;
-  height: 100%;
-  border-left-width: 1px;
-  border-left-style: solid;
-  border-left-color: black;
-  border-right-width: 1px;
-  border-right-style: solid;
-  border-right-color: black;
-  cursor: ew-resize;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  height: 3px;
+  width: 100%;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: black;
+  cursor: ns-resize;
   user-select: none;
 `;
 
-export const ResizableSidebar = () => {
+export const VerticalResizable = () => {
   const isMousePressedRef = useRef(false);
   const dividerRef = useRef<HTMLDivElement>(null);
-  const [leftW, setLeftW] = useState(200);
+  const [upH, setUpH] = useState(300);
 
   useEffect(() => {
     const mouseUpListener = () => {
@@ -29,12 +26,12 @@ export const ResizableSidebar = () => {
     const mouseMoveListener = (event: MouseEvent) => {
       if (isMousePressedRef.current) {
         if (dividerRef.current) {
-          if (event.x - 354 < 100) {
-            setLeftW(100);
-          } else if (event.x - 354 > 400) {
-            setLeftW(400);
+          if (event.y < 300) {
+            setUpH(300);
+          } else if (event.y  > 1000) {
+            setUpH(1000);
           } else {
-            setLeftW(event.x - 354);
+            setUpH(event.y);
           }
         }
       }
@@ -47,18 +44,17 @@ export const ResizableSidebar = () => {
       document.removeEventListener('mouseup', mouseUpListener);
       document.removeEventListener('mousemove', mouseMoveListener);
     }
-
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <Flex w="1200px" h="720px" bg="gray.200" p="10px">
-      <Box w={`${leftW}px`}>
+    <Flex w="300px" h="100vh" bg="gray.200" p="10px" direction="column">
+      <Box h={`${upH}px`}>
         Aside
       </Box>
       <Box ref={dividerRef} css={divider} onMouseDown={() => {
         isMousePressedRef.current = true;
       }} />
-      <Box w={`${1200 - leftW}px`}>
+      <Box css={css`height: 100vh - ${upH}`}>
         Main
       </Box>
     </Flex>
