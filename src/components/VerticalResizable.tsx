8e5ad01659e69e38d6d1a20002a7c96da08e7976
sitogi <takano162@gmail.com>
2021-08-06 23:06:57 +0900

    Update VerticalResaizable

--- src/components/VerticalResizable.tsx
+++ src/components/VerticalResizable.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Spacer } from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
 
 const divider = css`
@@ -18,6 +18,7 @@ export const VerticalResizable = () => {
   const isMousePressedRef = useRef(false);
   const dividerRef = useRef<HTMLDivElement>(null);
   const [upH, setUpH] = useState(300);
+  const [starredDisplayed, setStarredDisplayed] = useState(true);
 
   useEffect(() => {
     const mouseUpListener = () => {
@@ -26,13 +27,7 @@ export const VerticalResizable = () => {
     const mouseMoveListener = (event: MouseEvent) => {
       if (isMousePressedRef.current) {
         if (dividerRef.current) {
-          if (event.y < 300) {
-            setUpH(300);
-          } else if (event.y  > 1000) {
-            setUpH(1000);
-          } else {
-            setUpH(event.y);
-          }
+          setUpH(event.y - 160);
         }
       }
     };
@@ -43,20 +38,41 @@ export const VerticalResizable = () => {
     return () => {
       document.removeEventListener('mouseup', mouseUpListener);
       document.removeEventListener('mousemove', mouseMoveListener);
-    }
+    };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <Flex w="300px" h="100vh" bg="gray.200" p="10px" direction="column">
-      <Box h={`${upH}px`}>
-        Aside
+    <Box w="300px" h="100vh" bg="gray.200" p="10px">
+      <Box h={`150px`} borderBottom="solid 1px" mb="10px">
+        <Flex fontSize="24px" h="40px">
+          <Box>Crossing Boards</Box>
+        </Flex>
+        <div>Full-text</div>
+        <div>Starred</div>
+        <div>Recently</div>
+      </Box>
+      <Box>
+        <Flex fontSize="24px" h="40px">
+          <Box>Starred Boards</Box>
+          <Spacer />
+          <Box onClick={() => setStarredDisplayed(!starredDisplayed)}>x</Box>
+        </Flex>
+        <Box h={starredDisplayed ? `${upH}px` : '0px'} overflowY="auto">
+          {starredDisplayed && Array.from(Array(50).keys()).map(value => <div key={value}>{`starred ${value}`}</div>)}
+        </Box>
       </Box>
       <Box ref={dividerRef} css={divider} onMouseDown={() => {
         isMousePressedRef.current = true;
       }} />
-      <Box css={css`height: 100vh - ${upH}`}>
-        Main
+      <Box>
+        <Flex fontSize="24px" h="40px">
+          <Box>Boards</Box>
+        </Flex>
+        <Box css={css`height: calc(100vh - 200px - ${starredDisplayed ? upH : 35}px);
+          overflow-y: auto`}>
+          {Array.from(Array(100).keys()).map(value => <div key={value}>{`TreeItem ${value}`}</div>)}
+        </Box>
       </Box>
-    </Flex>
+    </Box>
   );
 };
