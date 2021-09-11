0220661ec409a842436ff55e7c974ff5fbec6744
sitogi <takano162@gmail.com>
2021-09-11 22:33:14 +0900

    Emotion でのスタイル定義の各方法と疑似要素などの確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Global } from '@emotion/react';
 import { globalStyle } from 'style';
-import { Box } from '@chakra-ui/react';
-import { VerticalResizable } from 'components/VerticalResizable';
+import { Flex } from '@chakra-ui/react';
+import { BeforeWithFlex } from 'components/BeforeWithFlex';
 
 function App() {
   return (
     <>
       <Global styles={globalStyle} />
-      <Box display="flex" w="100vw" alignItems="center" flexDirection="column">
-        <VerticalResizable />
-      </Box>
+      <Flex align="center" justify="center" h="100vh" w="100vw">
+        <BeforeWithFlex />
+      </Flex>
     </>
   );
 }
---
+++ src/components/BeforeWithFlex.tsx
@@ -0,0 +1,40 @@
+import { css } from '@emotion/react';
+import { Box, VStack } from '@chakra-ui/react';
+
+// after (before) 疑似要素について学ぶ
+export const BeforeWithFlex = () => {
+  return (
+    <VStack spacing="10px">
+      <Box css={css`
+        display: flex;
+        gap: 10px;
+
+        &::after {
+          content: '';
+          border: solid 10px;
+          background-color: black;
+        }
+      `}>
+        Emotion with String Style
+      </Box>
+      <Box css={{
+        display: 'flex',
+        gap: 10,
+        '&::after': {
+          content: '""',
+          border: 'solid 10px',
+          backgroundColor: 'black',
+        },
+      }}>
+        Emotion with Object Style
+      </Box>
+      <Box display="flex" gridGap="10px" _after={{
+        content: '""',
+        border: 'solid 10px',
+        backgroundColor: 'black',
+      }}>
+        Chakra
+      </Box>
+    </VStack>
+  );
+};
